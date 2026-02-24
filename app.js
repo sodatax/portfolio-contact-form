2a23bd791de383b3bf48697ab0d1238c6236bf63
sodatax <[200~62825932+sodatax@users.noreply.github.com~>
2026-02-23 17:04:48 -0800

    added confirmation form and admin

--- app.js
+++ app.js
@@ -5,15 +5,41 @@ import express from 'express';
 const app = express();
 
 // Define the port number where our server will listen
-const PORT = 3000;
+const PORT = 3003;
 
 app.use(express.static('public'));
+
+app.use(express.urlencoded({extended: true}));
+
+const info = [];
 
 // Define a default "route" ('/')
 // req: contains information about the incoming request
 // res: allows us to send back a response to the client
 app.get('/', (req, res) => {
     res.sendFile(`${import.meta.dirname}/views/home.html`)
+});
+
+///fname=Hosea&lname=Nacanaynay&job=&company=&linkedin=&email=&other=&message=
+app.post('/submit-form', (req,res) => {
+    const u_info = {
+        fname: req.body.fname,
+        lname: req.body.lname,
+        job: req.body.job,
+        company: req.body.company,
+        linkedin: req.body.linkedin,
+        email: req.body.email,
+        other: req.body.other,
+        message: req.body.message
+    };
+
+    info.push(u_info);
+
+    res.sendFile(`${import.meta.dirname}/views/confirmation.html`);
+});
+
+app.get('/admin', (req, res) => {
+    res.send(info);
 });
 
 // Start the server and listen on the specified port
